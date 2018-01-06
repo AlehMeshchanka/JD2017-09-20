a4794d1bfb04ccabffaabaa1f968e7cf3c8ed378
AlehMeshchanka <teraomol@gmail.com>
2018-01-06 13:27:12 +0300

    == operator

--- src/by/it/meshchenko/jd01_07/Runner07.java
+++ src/by/it/meshchenko/jd01_07/Runner07.java
@@ -5,34 +5,12 @@ import java.io.RandomAccessFile;
 
 public class Runner07 {
     public static void main(String[] args) {
-        // an array of creators
-        Creator[] creators = {new CreatorVar(), new CreatorVec()};
-        // iterate over creators and create products
-        for (Creator creator : creators) {
-            Var product = creator.factoryMethod();
-            System.out.printf("Created {%s}\n", product.getClass().getSimpleName());
-        }
+
+        int i3 = 1000;
+        int i4 = 1000;
+        if(i4==i3) System.out.println("true");
+        else System.out.println("false");
     }
-}
-
-class Var { }
-
-class VarScalar extends Var { }
-
-class VarVector extends Var { }
-
-abstract class Creator {
-    public abstract Var factoryMethod();
-}
-
-class CreatorVar extends Creator {
-    @Override
-    public Var factoryMethod() { return new VarScalar(); }
-}
-
-class CreatorVec extends Creator {
-    @Override
-    public Var factoryMethod() { return new VarVector(); }
 }
 
 
